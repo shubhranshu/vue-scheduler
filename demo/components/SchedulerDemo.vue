f286e8fd36af1e384c34c61b8e762f4e39fd7061
Shub <unset@examples.com>
2019-08-16 17:50:01 +1200

    Intermediate chnages

---
+++ demo/components/SchedulerDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout>
+    <v-flex xs12>
+      The one element
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
